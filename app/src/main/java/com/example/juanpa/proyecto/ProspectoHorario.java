5d2054943745fad7b3ac2ca8d55a374e5b184f19
Juanpamss <juanpa_mss@hotmail.com>
2017-08-12 22:51:08 -0500

    Commit

--- app/src/main/java/com/example/juanpa/proyecto/ProspectoHorario.java
+++ app/src/main/java/com/example/juanpa/proyecto/ProspectoHorario.java
@@ -1,13 +1,13 @@
 package com.example.juanpa.proyecto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class ProspectoHorario extends AppCompatActivity {
+public class ProspectoHorario extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prospecto_horario);
+        setContentView(new Calendario(this));
     }
 }
